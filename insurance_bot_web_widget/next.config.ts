0704cfd4b1dc06e21f03363a9559526d87c98cfb
Bhavya <bhavyakohli1209@gmail.com>
2026-08-24 10:09:51 +0530

    Update next.config.ts

--- insurance_bot_web_widget/next.config.ts
+++ insurance_bot_web_widget/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['460f-103-219-229-21.ngrok-free.app', 'https://insurancebotdemo.vercel.app'],
+  allowedDevOrigins: ['460f-103-219-229-21.ngrok-free.app', 'https://insurancebotdemo.vercel.app','insurancebotdemo.vercel.app'],
 };
 
 export default nextConfig;
