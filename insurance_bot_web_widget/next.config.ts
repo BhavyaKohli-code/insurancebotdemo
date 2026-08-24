4394ca0db1908099b644f1058807c7033a59b982
Bhavya <bhavyakohli1209@gmail.com>
2026-08-24 09:54:43 +0530

    bji

--- insurance_bot_web_widget/next.config.ts
+++ insurance_bot_web_widget/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['460f-103-219-229-21.ngrok-free.app'],
 };
 
 export default nextConfig;
